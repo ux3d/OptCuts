a83ec7300add2963a1cf7331a007ddda3c11a712
Moritz Willy Becher <becher@ux3d.io>
2024-07-22 15:12:09 +0200

    Load obj and start optimization (not optimizing)

--- tests/test_PyOptCuts.py
+++ tests/test_PyOptCuts.py
@@ -1,9 +1,11 @@
 import unittest
+import igl
 import PyOptCuts
 
 class TestPyOptCuts(unittest.TestCase):
     def test_optimize(self):
-        self.assertEqual(PyOptCuts.optimize(), 'expected result')
+        v, tc, n, f, ftc, fn = igl.read_obj("tests/bimba.obj")
+        self.assertEqual(PyOptCuts.optimize(v, tc, n, f, ftc, fn), 'expected result')
 
 if __name__ == '__main__':
     unittest.main()
