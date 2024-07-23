c4bd82b7305d1b282ed0798f4f80df77dc45224b
Moritz Willy Becher <becher@ux3d.io>
2024-07-23 12:41:18 +0200

    Expect optimize to return status code 0

--- tests/test_PyOptCuts.py
+++ tests/test_PyOptCuts.py
@@ -5,7 +5,7 @@ import PyOptCuts
 class TestPyOptCuts(unittest.TestCase):
     def test_optimize(self):
         v, tc, n, f, ftc, fn = igl.read_obj("tests/bimba.obj")
-        self.assertEqual(PyOptCuts.optimize(v, tc, n, f, ftc, fn), 'expected result')
+        self.assertEqual(PyOptCuts.optimize(v, tc, n, f, ftc, fn), 0)
 
 if __name__ == '__main__':
     unittest.main()
